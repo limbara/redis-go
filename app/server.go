c33b34671793457c6c9449f281d5d0801eb6ee0a
limbara <limbaranico@yahoo.com>
2024-04-11 21:21:03 +0700

    test 2nd stage

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -11,13 +12,31 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Error accepting connection: ", err)
+		}
+		conn.Close()
+	}()
+
+	_, err := io.WriteString(conn, "+PONG\r\n")
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
